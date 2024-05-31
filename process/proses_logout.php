aea2ff9104e4e5078cf88751be7895737b9d8421
Imam ariadi <gamerapin139@gmail.com>
2024-06-01 02:15:55 +0700

    awal

---
+++ process/proses_logout.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("../function/helper.php");
+require_once("../function/koneksi.php");
+
+
+session_start();
+session_unset();
+session_destroy();
+unset($_SESSION['id']);
+unset($_SESSION['role']);
+
+header("Location: ../index.php");
