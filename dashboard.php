ad77ff36446db9d22ef232159d3c27fea4dac533
Imam ariadi <gamerapin139@gmail.com>
2024-06-01 11:19:23 +0700

    update register

--- dashboard.php
+++ dashboard.php
@@ -15,9 +15,10 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Dashboard</title>
+   <link rel="stylesheet" href="assets/style01.css">
 </head>
 <body>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <nav class="navbar">
       <div class="container">
          <a class="navbar-brand" href="#">Multi User login</a>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
--- register.php
+++ register.php
@@ -16,24 +16,24 @@
 </head>
 <body>
     <div class="topbar">
-        <h3 class="text-topbar">Login sederhana</h3>
+        <h3 class="text-topbar">Halaman pendaftaran</h3>
     </div>
 
 
     <div class="content">
         <div class="card-login">
             <div class="card-main">
-                <div class="card-header">Form Login</div>
+                <div class="card-header">Form Pendaftaran</div>
                 <div class="card-body">
                     <form method="POST" action="<?= BASE_URL . 'process/process_login.php' ?>">
                     <label for="" class="form-label">Username</label><br>
                     <input type="username" name="username" class="form-input"><br>
                     <label for="" class="form-label">Password</label><br>
                     <input type="password" name="password" class="form-input"><br>
-                    <button type="submit" class="btn btn-login">Login</button>
+                    <button type="submit" name="daftar" class="btn-login">Daftar</button>
                     </form>
                 </div>
-                <p class="caption">Belum Memiliki akun? <a href="<?= BASE_URL . 'register.php' ?>">Daftar</a></p>
+                <p class="caption">Sudah Memiliki akun? <a href="<?= BASE_URL . 'index.php' ?>">Login</a></p>
             </div>
         </div>
     </div>
